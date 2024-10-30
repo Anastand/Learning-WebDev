b9c442bcf42b9cb138ec93fecb0f2f535dccaefa
Aryan_Bhardwaj <bhardwaj03aryan@gmail.com>
2024-10-30 12:37:03 +0530

    Update expresslearning.js

--- 100xDEV/jslearning/expresslearning.js
+++ 100xDEV/jslearning/expresslearning.js
@@ -1,9 +1,18 @@
 
 const express = require('express');
 
+function square(n){
+    return n*n;
+}
+
+
 const app = express();
 
-
+app.get("/", function(req,res){
+    const n = req.query.n;
+    const value = square(n);
+    res.send(`The square of ${n} is ${value}`);
+})
 
 app.listen(3000);
 
